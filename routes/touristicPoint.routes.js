a85481c0ff7dc7964ed792e63e397761c9459888
Filipa Flora <filipa.flora@gmail.com>
2023-08-15 12:43:04 +0100

    Added edit and delete actions in TouristicPoints

--- routes/touristicPoint.routes.js
+++ routes/touristicPoint.routes.js
@@ -6,6 +6,8 @@ const City = require("../models/City.model.js");
 const User = require("../models/User.model.js");
 const TouristicPoint = require("../models/TouristicPoint.model.js");
 
+// GET ROUTE TO CREATE TOURISTIC POINTS
+
 router.get("/cities/:cityId/createTouristicPoint", async (req,res) => {
   try {
     const {cityId} = req.params;
@@ -16,6 +18,8 @@ router.get("/cities/:cityId/createTouristicPoint", async (req,res) => {
     console.log(error);
   }
 });
+
+// POST ROUTE TO SUBMIT INFORMATION ABOUT THE CREATED TOURISTIC POINT
 
 router.post("/cities/:cityId/createTouristicPoint", async (req, res) => {
   try {
@@ -31,6 +35,59 @@ router.post("/cities/:cityId/createTouristicPoint", async (req, res) => {
     });
 
     res.redirect(`/cities/${cityId}#${newTouristicPoint._id}`);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// GET ROUTE TO EDIT DETAILS ON TOURISTIC POINT
+
+router.get("/cities/:cityId/touristicPoints/:touristicPointId/edit", async (req, res) => {
+  try {
+    const { cityId, touristicPointId } = req.params;
+    let foundTouristicPoint = await TouristicPoint.findById(touristicPointId);
+    res.render("touristicPoints/touristicPoint-edit.hbs", { touristicPoint: foundTouristicPoint, cityId });
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// POST ROUTE TO MAKE UPDATES ON A SPECIFIC TOURISTIC POINT 
+
+router.post("/cities/:cityId/touristicPoints/:touristicPointId/edit", async (req, res) => {
+  try {
+   
+    const {cityId} = req.params
+    const { touristicPointId } = req.params;
+    const { name, description } = req.body;
+
+
+    await TouristicPoint.findByIdAndUpdate(touristicPointId, { name, description });
+
+    console.log("test");
+    // redirect to the specific city list page
+    res.redirect(`/cities/${cityId}`);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// GET ROUTE TO DELETE TOURISTIC POINT
+
+router.post("/cities/:cityId/deleteTouristicPoint/:touristicPointId", async (req, res) => {
+  try {
+    const {cityId} = req.params
+    const { touristicPointId } = req.params;
+
+    const removeTouristicPoint = await TouristicPoint.findByIdAndRemove(touristicPointId)
+    
+    await City.findByIdAndUpdate(cityId, {$pull: {touristicPoints: removeTouristicPoint._id}});
+  
+    res.redirect(`/cities/${cityId}`);
+
 
   } catch (error) {
     console.log(error);
